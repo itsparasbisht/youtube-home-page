57319bb34ab7ae49da7d69d6153389737f382d74
itsparasbisht <iamparasbisht@gmail.com>
2023-10-17 20:48:13 +0530

    category pills slider logic init

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,3 +1,5 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useState } from "react";
 import Button from "./Button";
 
 type CategoryPillsProps = {
@@ -6,14 +8,23 @@ type CategoryPillsProps = {
   onSelect: (category: string) => void;
 };
 
+const TRANSLATE_AMOUNT = 200;
+
 export default function CategoryPills({
   categories,
   selectedCategory,
   onSelect,
 }: CategoryPillsProps) {
+  const [translate, setTranslate] = useState(0);
+  const [isLeftVisible, setIsLeftVisible] = useState(true);
+  const [isRightVisible, setIsRightVisible] = useState(false);
+
   return (
     <div className="overflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+      <div
+        className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map((category) => (
           <Button
             key={category}
@@ -25,6 +36,35 @@ export default function CategoryPills({
           </Button>
         ))}
       </div>
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className="h-full aspect-square w-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_AMOUNT;
+                // if (newTranslate <= 0) return 0;
+                return newTranslate;
+              });
+            }}
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
