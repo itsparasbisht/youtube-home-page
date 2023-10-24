7bd7a64e07fde26bc8dda4b31d536701c6baed10
itsparasbisht <iamparasbisht@gmail.com>
2023-10-24 20:21:17 +0530

    LargeSideBarSection component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,30 @@
 import { Clapperboard, Home, Library, Repeat } from "lucide-react";
-import { ElementType } from "react";
+import { Children, ElementType, ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 import { buttonStyles } from "./Button";
 
 export default function Sidebar() {
   return (
-    <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
-      <SmallSidebarItem Icon={Home} title="Home" url="/" />
-      <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
-      <SmallSidebarItem
-        Icon={Clapperboard}
-        title="Subscriptions"
-        url="/subscriptions"
-      />
-      <SmallSidebarItem Icon={Library} title="Library" url="/library" />
-    </aside>
+    <>
+      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+        <SmallSidebarItem Icon={Home} title="Home" url="/" />
+        <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+        <SmallSidebarItem
+          Icon={Clapperboard}
+          title="Subscriptions"
+          url="/subscriptions"
+        />
+        <SmallSidebarItem Icon={Library} title="Library" url="/library" />
+      </aside>
+      <aside className="w-56 lg:sticky absolute top-0 overflow-y-hidden scrollbar-hidden flex flex-col pb-4 gap-2 px-2">
+        <LargeSidebarSection title="Slice 1" visibleItemCount={1}>
+          <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem Icon={Home} title="Home" url="/" />
+        </LargeSidebarSection>
+      </aside>
+    </>
   );
 }
 
@@ -35,6 +45,58 @@ function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
     >
       <Icon className="w-6 h-6" />
       <div className="text-sm">{title}</div>
+    </a>
+  );
+}
+
+type LargeSidebarSectionProps = {
+  children: ReactNode;
+  title?: string;
+  visibleItemCount?: number;
+};
+
+function LargeSidebarSection({
+  children,
+  title,
+  visibleItemCount = Number.POSITIVE_INFINITY,
+}: LargeSidebarSectionProps) {
+  const childrenArray = Children.toArray(children).flat();
+  const visibleChildren = childrenArray.slice(0, visibleItemCount);
+  return (
+    <div>
+      {title && <div className="ml-4 mt-2 text-lg mb-1">{title}</div>}
+      {visibleChildren}
+    </div>
+  );
+}
+
+type LargeSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+  isActive?: boolean;
+};
+
+function LargeSidebarItem({
+  Icon,
+  title,
+  url,
+  isActive = false,
+}: LargeSidebarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        `w-full flex items-center rounded-lg gap-4 p-3 ${
+          isActive ? "font-bold bg-neutral-100 hover:bg-secondary" : undefined
+        }`
+      )}
+    >
+      <Icon className="w-6 h-6" />
+      <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+        {title}
+      </div>
     </a>
   );
 }
